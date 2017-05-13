2dc6a514b13d09772ef45e5d92b30ada097a27ee
Mai Hoang <mnhoang.80@gmail.com>
2017-05-13 13:47:17 -0700

    Add users-controller & integrate passport

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 module.exports = function(app){
-	
-	var buyer_controller = require("./controllers/buyer-controller.js");
-	var admin_controller = require("./controllers/admin-controller.js")
+
+	var users_controller = require("./controllers/users-controller.js");
+	// var admin_controller = require("./controllers/admin-controller.js");
 	// var prod_controller = require("./controllers/product-controller.js");
 
-	app.use("/members", buyer_controller);
-	app.use("/admin", admin_controller);
+	app.use("/members", users_controller);
+	// app.use("/admin", admin_controller);
 
 }
