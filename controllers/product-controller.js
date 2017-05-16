15cff6a15af4ab0e0a3cdc163ce969496f1ac96a
Marcos Lee <marcosl8686@gmail.com>
2017-05-15 20:04:41 -0700

    update product-controller to handle handlebars correctly

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -2,11 +2,13 @@ var db  = require('../models');
 var express = require('express');
 var router  = express.Router();
 var isAuthenticated = require("../config/middleware/isAuthenticated");
+var product = require("../models/product.js");
 
-router.get("/members", isAuthenticated, function(req, res){
-	res.render("product", {
-		layout: "main-product"
-	});
+router.get('/members', function(req,res) {
+	db.Products.findAll({}).then(function(data) {
+		res.render("product", {products: data});
+	})
 });
+
 
 module.exports = router;
