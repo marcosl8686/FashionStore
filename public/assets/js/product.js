d7ee22ee18cc176a8bce9c98280a4b5c2b6937d8
Mai Hoang <mnhoang.80@gmail.com>
2017-05-19 20:48:05 -0700

    Connect modal to cart

--- public/assets/js/product.js
+++ public/assets/js/product.js
@@ -1,6 +1,9 @@
 $('.modalSelected').click(function(){
 	var path = '/product/rendermodal/' + $(this).attr('id');
+	console.log("THIS HERE",this);
 	$.get(path).then(function(res){
+	console.log("THIS NOW", res);
+
 		$('#product-modal').html(res);
 		$('#product-modal').modal('show');
 	})
