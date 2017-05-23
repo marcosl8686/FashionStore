0106e7279d1956ce5ef11fb3949d5dbd6ced773b
navarretejuan <navarretejuan09@gmail.com>
2017-05-22 20:03:43 -0700

    add production link

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+/**
+ * Created by juancarlosnavarrete on 5/21/17.
+ */
+var mysql = require('mysql');
+var connection;
+
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '', // Add your password
+        database : 'jolie' // Add your database
+    });
+}
+
+
+// Export the Connection
+module.exports = connection;
